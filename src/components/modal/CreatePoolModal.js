80f181be6aa281f410aafa6d38ec4e3fd0bf3aab
sunghoon <sunghoon@bharvest.io>
2021-01-06 20:27:30 +0900

    [feat] replace creat pool elements to common elements

--- src/components/modal/CreatePoolModal.js
+++ src/components/modal/CreatePoolModal.js
@@ -3,16 +3,17 @@ import { Component } from 'react';
 import { txGenerator } from '../../common/cosmos-amm'
 import { currencies } from '../../common/config'
 import BlackOverLay from '../overlays/BlackOverLay'
+import TokenSetter from '../../elements/TokenSetter';
 
 
 class CreatePoolModal extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            tokenX: currencies[0].coinMinimalDenom,
-            tokenY: currencies[1].coinMinimalDenom,
-            tokenXAmount: '',
-            tokenYAmount: '',
+            tokenA: currencies[0].coinMinimalDenom,
+            tokenB: currencies[1].coinMinimalDenom,
+            tokenAAmount: '',
+            tokenBAmount: '',
         };
     }
 
@@ -24,20 +25,20 @@ class CreatePoolModal extends Component {
     }
     // 로직 함수 시작
     createPool = async () => {
-        console.log(`X : ${this.state.tokenX} ${this.state.tokenXAmount}`)
-        console.log(`Y : ${this.state.tokenY} ${this.state.tokenYAmount}`)
+        console.log(`X : ${this.state.tokenA} ${this.state.tokenAAmount}`)
+        console.log(`Y : ${this.state.tokenB} ${this.state.tokenBAmount}`)
 
-        const tokenX = this.state.tokenX
-        const tokenY = this.state.tokenY
-        const amountX = Math.floor(Number(this.state.tokenXAmount) * 1000000);
-        const amountY = Math.floor(Number(this.state.tokenYAmount) * 1000000);
+        const tokenA = this.state.tokenA
+        const tokenB = this.state.tokenB
+        const amountX = Math.floor(Number(this.state.tokenAAmount) * 1000000);
+        const amountY = Math.floor(Number(this.state.tokenBAmount) * 1000000);
 
-        const arrangedReserveCoinDenoms = sortReserveCoinDenoms(tokenX, tokenY)
+        const arrangedReserveCoinDenoms = sortReserveCoinDenoms(tokenA, tokenB)
 
         const msgData = {
             pool_type_index: 1,
             reserve_coin_denoms: arrangedReserveCoinDenoms,
-            deposit_coins: getDepositCoins(arrangedReserveCoinDenoms, { [tokenX]: amountX, [tokenY]: amountY })
+            deposit_coins: getDepositCoins(arrangedReserveCoinDenoms, { [tokenA]: amountX, [tokenB]: amountY })
         }
 
         const feeData = {
@@ -92,25 +93,47 @@ class CreatePoolModal extends Component {
             <>
                 <BlackOverLay modalHandler={this.props.modalHandler} />
                 <Modal >
-                    <ReserveTokenCard>
+                    <TokenSetter
+                        currencies={currencies}
+                        leftTitle="Token A"
+                        rightTitle="current balance:"
+                        cssId="A"
+                        token={this.state.tokenA}
+                        tokenAmount={this.tokenAAmount}
+                        selectorHandler={this.tokenSelectorChangeHandler}
+                        amountHandler={this.amountChangeHandler}
+                        cssStyle={{ marginBottom: "20px" }}
+                    />
+
+                    <TokenSetter
+                        currencies={currencies}
+                        leftTitle="Token B"
+                        rightTitle="current balance:"
+                        cssId="B"
+                        token={this.state.tokenB}
+                        tokenAmount={this.tokenBAmount}
+                        selectorHandler={this.tokenSelectorChangeHandler}
+                        amountHandler={this.amountChangeHandler} />
+
+                    {/* <ReserveTokenCard>
                         <TokenTitle>Reserve Token X</TokenTitle>
-                        <TokenSelector id="tokenX" value={this.state.tokenX} onChange={this.tokenSelectorChangeHandler}>
+                        <TokenSelector id="tokenA" value={this.state.tokenA} onChange={this.tokenSelectorChangeHandler}>
                             {this.createOptions(currencies)}
                         </TokenSelector>
                         <TokenTitle>Deposit Amount </TokenTitle>
-                        <DepositInput id="tokenXAmount" placeholder="0" value={this.state.tokenXAmount} onChange={this.amountChangeHandler}></DepositInput>
+                        <DepositInput id="tokenAAmount" placeholder="0" value={this.state.tokenAAmount} onChange={this.amountChangeHandler}></DepositInput>
                     </ReserveTokenCard>
 
                     <Divider />
 
                     <ReserveTokenCard>
                         <TokenTitle>Reserve Token Y</TokenTitle>
-                        <TokenSelector id="tokenY" value={this.state.tokenY} onChange={this.tokenSelectorChangeHandler}>
+                        <TokenSelector id="tokenB" value={this.state.tokenB} onChange={this.tokenSelectorChangeHandler}>
                             {this.createOptions(currencies)}
                         </TokenSelector>
                         <TokenTitle>Deposit Amount </TokenTitle>
-                        <DepositInput id="tokenYAmount" placeholder="0" value={this.state.tokenYAmount} onChange={this.amountChangeHandler}></DepositInput>
-                    </ReserveTokenCard>
+                        <DepositInput id="tokenBAmount" placeholder="0" value={this.state.tokenBAmount} onChange={this.amountChangeHandler}></DepositInput>
+                    </ReserveTokenCard> */}
 
                     <CreateNewPoolButton onClick={this.createPool}>Create New Pool</CreateNewPoolButton>
                 </Modal>
@@ -121,58 +144,59 @@ class CreatePoolModal extends Component {
 
 const Modal = styled.div`
     position:absolute;
-    width: 500px;
-    height: 600px;
+    width: 460px;
+    height: 560px;
+    padding: 20px;
     background-color:#fff;
     transform: translate(-50%, -50%);
     top: 50%;
     left: 50%;
     border-radius: 8px;
 `
-const ReserveTokenCard = styled.section`
-    width: 324px;
-    margin: 0 auto;
-    border: 1px solid gray;
-    border-radius: 8px;
-    text-align:left;
-    padding: 20px;
-`
-const TokenTitle = styled.div`
-    font-weight: 700;
-    font-size: 18px;
-    margin-bottom: 8px;
-`
+// const ReserveTokenCard = styled.section`
+//     width: 324px;
+//     margin: 0 auto;
+//     border: 1px solid gray;
+//     border-radius: 8px;
+//     text-align:left;
+//     padding: 20px;
+// `
+// const TokenTitle = styled.div`
+//     font-weight: 700;
+//     font-size: 18px;
+//     margin-bottom: 8px;
+// `
 
-const TokenSelector = styled.select` 
-    padding: 0 12px;
-    cursor: pointer;
-    border-radius: 8px;
-    height: 32px;
-    width: 326px;
-    font-weight: 700;
-    line-height: 32px;
-    border: 1px solid gray;
-    margin-bottom: 20px;
-    &:hover {
-        font-weight: 700;
-    }
-`
+// const TokenSelector = styled.select` 
+//     padding: 0 12px;
+//     cursor: pointer;
+//     border-radius: 8px;
+//     height: 32px;
+//     width: 326px;
+//     font-weight: 700;
+//     line-height: 32px;
+//     border: 1px solid gray;
+//     margin-bottom: 20px;
+//     &:hover {
+//         font-weight: 700;
+//     }
+// `
 
-const DepositInput = styled.input`
-    padding: 0 12px;
-    cursor: pointer;
-    border-radius: 8px;
-    height: 32px;
-    width: 300px;
-    font-weight: 700;
-    line-height: 32px;
-    border: 1px solid gray;
-`
-const Divider = styled.div`
-    height: 20px;
-    width: 50%;
-    border-right: 1px solid gray;
-`
+// const DepositInput = styled.input`
+//     padding: 0 12px;
+//     cursor: pointer;
+//     border-radius: 8px;
+//     height: 32px;
+//     width: 300px;
+//     font-weight: 700;
+//     line-height: 32px;
+//     border: 1px solid gray;
+// `
+// const Divider = styled.div`
+//     height: 20px;
+//     width: 50%;
+//     border-right: 1px solid gray;
+// `
 
 const CreateNewPoolButton = styled.div`
     margin: 20px auto 0 auto;
---
+++ src/elements/TokenSetter.js
@@ -0,0 +1,114 @@
+import { Component } from 'react';
+import styled from 'styled-components';
+
+class TokenSetter extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            tokenX: props.currencies[0].coinMinimalDenom,
+            tokenY: props.currencies[1].coinMinimalDenom,
+            tokenXAmount: '',
+            tokenYAmount: '',
+        }
+
+    }
+
+    componentDidMount() {
+        console.log(this.props.currencies)
+    }
+
+
+    createOptions(data) {
+        return (
+            data.map((item) => {
+                return (
+                    <option value={item.coinMinimalDenom} key={item.coinDenom}>{item.coinDenom}</option>
+                )
+            }
+            )
+        )
+    }
+    tokenSelectorChangeHandler = (e) => {
+        this.setState({
+            [e.target.id]: e.target.value
+        })
+    }
+
+    amountChangeHandler = (e) => {
+        this.setState({
+            [e.target.id]: e.target.value
+        })
+    }
+
+    render() {
+        return (
+            <>
+                <TokenCard style={this.props.cssStyle}>
+                    <Left>
+                        <Title>{this.props.leftTitle ? this.props.leftTitle : "leftTitle"}</Title>
+                        <TokenSelector id={`token${this.props.cssId}`} value={this.props.token} onChange={this.props.selectorHandler}>
+                            {this.createOptions(this.props.currencies)}
+                        </TokenSelector>
+                    </Left>
+                    <Right>
+                        <Title>{this.props.rightTitle ? `${this.props.rightTitle}` : 'rightTitle'} </Title>
+                        <DepositInput id={`token${this.props.cssId}Amount`} placeholder="0" value={this.props.tokenAmount} onChange={this.props.amountHandler}></DepositInput>
+                    </Right>
+                </TokenCard>
+            </>
+        )
+    }
+}
+
+const TokenCard = styled.section`
+
+margin: 0 auto;
+
+border-radius: 8px;
+border: 2px solid black;
+padding: 10px 20px;
+display:flex;
+`
+const Title = styled.div`
+font-weight: 300;
+font-size: 12px;
+color:#777777;
+margin-bottom: 8px;
+`
+
+const Left = styled.div`
+    flex:1;
+    text-align:left;
+    height: 60px;
+`
+const Right = styled(Left)`
+    text-align:right;
+`
+
+const TokenSelector = styled.select` 
+    padding: 0 ;
+    cursor: pointer;
+    border-radius: 8px;
+    height: 32px;
+    font-weight: 700;
+    font-size:18px;
+    line-height: 32px;
+    border: none;
+    outline: none;
+`
+
+const DepositInput = styled.input`
+    padding: 0 12px;
+    cursor: pointer;
+    border-radius: 8px;
+    height: 32px;
+    font-size:18px;
+    font-weight: 700;
+    line-height: 32px;
+    border: none;
+    text-align:right;
+    outline: none;
+`
+
+export default TokenSetter
