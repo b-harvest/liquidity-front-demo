8e046bef6d95859febc38ef1d92377b713d14dac
beardoggo <2ngddo@gmail.com>
2021-01-07 18:47:18 +0900

    Relocated "Select" buttons into a new column in PoolList component

--- src/components/PoolList.js
+++ src/components/PoolList.js
@@ -3,7 +3,6 @@ import { Component } from "react";
 import styled from "styled-components";
 
 class PoolList extends Component {
-<<<<<<< HEAD
 	constructor(props) {
 		super(props);
 		this.state = {
@@ -63,21 +62,20 @@ class PoolList extends Component {
 				const secondPairPrice = this.getSecondPairPrice(item);
 				return (
 					<Row key={index}>
+						<div>{`${pairs[0]}-${pairs[1]}`}</div>
 						<div>
-							{`${pairs[0]}-${pairs[1]}`}
+							{`1 ${pairs[0]} per`}
 							<br />
+							{`${secondPairPrice} ${pairs[1]}`}
+						</div>
+						<div>
 							<DepositButton
 								onClick={() => {
 									this.selectPool(item);
 								}}
 							>
-								Deposit
+								{this.props.actionName ? this.props.actionName : "Select"}
 							</DepositButton>
-						</div>
-						<div>
-							{`1 ${pairs[0]} per`}
-							<br />
-							{`${secondPairPrice} ${pairs[1]}`}
 						</div>
 					</Row>
 				);
@@ -92,94 +90,31 @@ class PoolList extends Component {
 					<TableHeader>
 						<div>Pool</div>
 						<div>Price</div>
+						<div>Action</div>
 					</TableHeader>
 					{this.createRows(this.state.poolData)}
 				</PoolTable>
 			</>
 		);
 	}
-=======
-    constructor(props) {
-        super(props);
-        this.state = {
-            poolData: null,
-            updatePool: null,
-        };
-    }
-
-    componentDidMount() {
-        const initGetPoolList = async () => {
-            try {
-                const poolList = await getPoolList()
-                this.setState({ poolData: poolList })
-            } catch (error) {
-                console.error(error)
-            }
-        }
-
-        initGetPoolList()
-        const updatePool = setInterval(() => { initGetPoolList() }, 5000)
-        this.setState({ updatePool: updatePool })
-    }
-    componentWillUnmount() {
-        clearInterval(this.state.updatePool)
-    }
-
-    getPoolPairs(item) {
-        return [item.liquidity_pool.reserve_coin_denoms[0].substr(1).toUpperCase(), item.liquidity_pool.reserve_coin_denoms[1].substr(1).toUpperCase()]
-    }
-
-    getSecondPairPrice(item) {
-        const price = Number(item.liquidity_pool_metadata.reserve_coins[1].amount) / Number(item.liquidity_pool_metadata.reserve_coins[0].amount)
-        return Number(price).toFixed(2)
-    }
-
-    selectPool = (pool) => {
-        this.props.selectPool(pool)
-    }
-
-    createRows = (data) => {
-        if (data === null) {
-            return (<div style={{ color: "#ea5353", fontSize: "18px", fontWeight: "bold" }}></div>)
-        } else {
-            return (
-                data.map((item, index) => {
-                    const pairs = this.getPoolPairs(item)
-                    const secondPairPrice = this.getSecondPairPrice(item)
-                    return (
-                        <Row key={index}>
-                            <div>{`${pairs[0]}-${pairs[1]}`}<br /><DepositButton onClick={() => { this.selectPool(item) }}>{this.props.actionName ? this.props.actionName : "Select"}</DepositButton></div>
-                            <div>{`1 ${pairs[0]} per`}<br />{`${secondPairPrice} ${pairs[1]}`}</div>
-                        </Row>)
-                })
-            )
-        }
-    }
-
-    render() {
-        return (
-            <>
-                <PoolTable>
-                    <TableHeader>
-                        <div>Pool</div>
-                        <div>Price</div>
-                    </TableHeader>
-                    {this.createRows(this.state.poolData)}
-                </PoolTable>
-            </>
-        )
-    }
->>>>>>> refs/remotes/origin/main
 }
 const Row = styled.div`
 	margin-bottom: 20px;
 	display: flex;
-	width: 540px;
+	width: 660px;
+
+	div:first-child {
+		line-height: 48px;
+	}
 
 	div {
-		width: 50%;
+		width: 33.3333%;
 		display: inline-block;
 		line-height: 24px;
+	}
+
+	div:last-child {
+		line-height: 48px;
 	}
 `;
 const PoolTable = styled.section`
