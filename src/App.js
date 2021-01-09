57a4aac325e419bbb55c687be78a6a79262e5ab7
sunghoon <sunghoon@bharvest.io>
2021-01-10 00:09:16 +0900

    [chore] update favicon & web page title

--- src/App.js
+++ src/App.js
@@ -2,73 +2,12 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  // Link
 } from "react-router-dom";
-//import axios from 'axios';
 import BasicLayout from './components/layouts/BasicLayout'
 import Pools from './pages/Pools'
 import Deposit from './pages/Deposit'
 import Withdraw from './pages/Withdraw'
 import Swap from './pages/Swap'
-
-const myTokenData = [
-  {
-    reserveOne: 'TKNA',
-    reserveTwo: 'TKNB',
-    myToken: {
-      balance: 55,
-      percentage: 17.5
-    }
-  },
-  {
-    reserveOne: 'TKNC',
-    reserveTwo: 'TKND',
-    myToken: {
-      balance: 20,
-      percentage: 7.51
-    }
-  },
-  {
-    reserveOne: 'TKNE',
-    reserveTwo: 'TKNF',
-    myToken: {
-      balance: 15,
-      percentage: 1.5
-    }
-  }
-]
-
-const tokenList = [
-  'ATOM',
-  'IRIS',
-  'SCRT',
-  'AMM'
-]
-
-const poolInfo = {
-  reserveTokenX: {
-    denom: 'USDT',
-    balance: 4520,
-  },
-  reserveTokenY: {
-    denom: 'ATOM',
-    balance: 1000,
-  },
-  latestSwapPrice: {
-    XY: 0.2212,
-    YX: 0.2217
-  },
-  poolTokenSupply: 100
-}
-
-const withdrawInfo = {
-  pools: ['ATOM-USDT', "ATOM-USDC"],
-  myPoolToken: 10
-}
-
-
-
-
 
 function App() {
   return (
@@ -76,16 +15,16 @@ function App() {
       <BasicLayout>
         <Switch>
           <Route exact path="/">
-            <Pools myTokenData={myTokenData} />
+            <Pools />
           </Route>
           <Route exact path="/deposit">
-            <Deposit tokenList={tokenList} poolInfo={poolInfo} />
+            <Deposit />
           </Route>
           <Route exact path="/withdraw">
-            <Withdraw tokenList={tokenList} withdrawInfo={withdrawInfo} poolInfo={poolInfo} />
+            <Withdraw />
           </Route>
           <Route exact path="/swap">
-            <Swap tokenList={tokenList} poolInfo={poolInfo} />
+            <Swap />
           </Route>
         </Switch>
       </BasicLayout>
