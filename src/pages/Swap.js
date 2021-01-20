1ca3fe1de58d7bcb330318a2607d938b6412f43b
sunghoon <sunghoon@bharvest.io>
2021-01-20 17:23:14 +0900

    [fix] swap js useState & refactor swap function

--- src/pages/Swap.js
+++ src/pages/Swap.js
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import { useEffect, useState } from "react";
 import { Wrapper, Detail, GoBack, DepositCard } from "../design/pages/Swap";
 
 import { currencies } from "../common/config";
@@ -10,70 +10,48 @@ import TokenSetter from "../elements/TokenSetter";
 import ChangeButton from "../elements/ChangeButton";
 import BasicButtonCard from "../elements/BasicButtonCard";
 
-class Swap extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            tokenA: "",
-            tokenB: "",
-            tokenAAmount: "",
-            tokenBAmount: "",
-            tokenAPoolAmount: "",
-            tokenBPoolAmount: "",
-            poolId: "",
-            poolTypeIndex: "",
-            tokenIndexer: this.props.data.tokenIndexer,
-            slippage: 0,
-            isLoading: false,
-            isExceeded: false,
-            isPoolSelected: false
-        };
-    }
+function Swap(props) {
+    const [tokenA, setTokenA] = useState("")
+    const [tokenB, setTokenB] = useState("")
+    const [tokenAAmount, setTokenAAmount] = useState("")
+    const [tokenBAmount, setTokenBAmount] = useState("")
+    const [tokenAPoolAmount, setTokenAPoolAmount] = useState("")
+    const [tokenBPoolAmount, setTokenBPoolAmount] = useState("")
+    const [poolId, setPoolId] = useState("")
+    const [poolTypeIndex, setPoolTypeIndex] = useState("")
+    const [tokenIndexer, setTokenIndexer] = useState(props.data.tokenIndexer)
+    const [slippage, setSlippage] = useState(0)
+    const [isLoading, setIsLoading] = useState(false)
+    const [isExceeded, setIsExceeded] = useState(false)
+    const [isPoolSelected, setIsPoolSelected] = useState(false)
 
-    componentDidMount() {
-        if (this.props.data.tokenIndexer !== null) {
+    useEffect(() => {
+        if (props.data.tokenIndexer !== null) {
             try {
-                this.setState({ tokenIndexer: this.props.data.tokenIndexer });
+                setTokenIndexer(props.data.tokenIndexer)
             } catch (error) {
                 console.error(error);
             }
         }
-    }
+    }, [props.data.tokenIndexer])
 
-    componentDidUpdate(prevProps) {
-        if (prevProps.data.tokenIndexer !== this.props.data.tokenIndexer) {
-            try {
-                this.setState({ tokenIndexer: this.props.data.tokenIndexer });
-            } catch (error) {
-                console.error(error);
-            }
-        }
-    }
 
     // 로직 함수 시작
-    swap = async () => {
-        console.log(`From : ${this.state.tokenA} ${this.state.tokenAAmount}`);
-        console.log(`To : ${this.state.tokenB} ${this.state.tokenBAmount}`);
-
-        const tokenA = this.state.tokenA;
-        const tokenB = this.state.tokenB;
-        const amountA = Math.floor(Number(this.state.tokenAAmount) * 1000000);
+    async function swap() {
+        const amountA = Math.floor(Number(tokenAAmount) * 1000000);
         const params = await getLiquidityModuleParams() // for swap_fee_rate
-
-        console.log(params)
-
         let orderPrice = null
 
         //is reverse?
         if ([tokenA, tokenB].sort()[0] !== tokenA) {
-            orderPrice = Number(this.state.tokenBPoolAmount) / Number(this.state.tokenAPoolAmount) * 0.9
+            orderPrice = Number(tokenBPoolAmount) / Number(tokenAPoolAmount) * 0.9
         } else {
-            orderPrice = Number(this.state.tokenAPoolAmount) / Number(this.state.tokenBPoolAmount) * 1.1
+            orderPrice = Number(tokenAPoolAmount) / Number(tokenBPoolAmount) * 1.1
         }
 
         const msgData = {
-            poolId: this.state.poolId,
-            poolTypeIndex: this.state.poolTypeIndex,
+            poolId: poolId,
+            poolTypeIndex: poolTypeIndex,
             swapType: 1,
             offerCoin: {
                 denom: tokenA,
@@ -86,7 +64,6 @@ class Swap extends Component {
             demandCoinDenom: tokenB,
             orderPrice: Number(orderPrice).toFixed(18)
         };
-        console.log('OrderPrice', msgData.orderPrice);
 
         const feeData = {
             denom: "uatom",
@@ -94,54 +71,63 @@ class Swap extends Component {
             gas: "180000"
         };
 
+        console.log(`From : ${tokenA} ${tokenAAmount}`)
+        console.log(`To : ${tokenB} ${tokenBAmount}`)
+        console.log('OrderPrice', msgData.orderPrice);
+        console.log(`Swap fee rate : ${params.swap_fee_rate}`)
+
         try {
-            this.setState({ isLoading: true });
+            setIsLoading(true)
             const response = await txGenerator("MsgSwap", msgData, feeData);
-            this.setState({ isLoading: false });
+            setIsLoading(false)
             if (String(response).includes("Error")) {
                 throw response;
             }
-            this.reset();
+            reset();
             // alert("Your tokens have been swapped successfully");
         } catch (error) {
             alert(error);
-            this.setState({ isLoading: false });
+            setIsLoading(false)
         }
     };
     // 로직 함수 끝
 
-    tokenSelectorChangeHandler = (e) => {
-        this.setState({
-            [e.target.id]: e.target.value
-        });
+    function tokenSelectorChangeHandler(e) {
+        // setDynamicValues({ ...dynamicValues, [e.target.id]: e.target.value })
     };
 
-    amountChangeHandler = (e) => {
-        const isReverse = e.target.id === "tokenAAmount" ? false : true
-        const tokenA = isReverse ? this.state.tokenB : this.state.tokenA
-        const tokenB = isReverse ? this.state.tokenA : this.state.tokenB
-
-        const slippage = calculateSlippage(e.target.value * 1000000, this.state.tokenAPoolAmount)
-        const indexer = this.state.tokenIndexer
-
+    function amountChangeHandler(e) {
         const swapAmount = e.target.value
-        const tokenAAmount = getMyTokenBalanceNumber(tokenA, indexer)
-        const tokenBAmount = getMyTokenBalanceNumber(tokenB, indexer)
-        const { counterPair, counterPairAmount } = calculateCounterPairAmount(e, this.state, "swap")
+        const myATokenBalance = getMyTokenBalanceNumber(tokenA, tokenIndexer)
+        const isReverse = e.target.id === "tokenAAmount" ? false : true
+        const slippage = calculateSlippage(swapAmount * 1000000, isReverse ? tokenBPoolAmount : tokenAPoolAmount)
+        const state = { tokenAPoolAmount, tokenBPoolAmount }
 
         let isExceeded = false
+        let { counterPairAmount } = calculateCounterPairAmount(e, state, "swap")
+        counterPairAmount = Math.abs(Number(counterPairAmount).toFixed(2))
 
         // is exceeded?(좌변에 fee 더해야함)
-        if (isReverse ? counterPairAmount > tokenBAmount : swapAmount > tokenAAmount) {
-            isExceeded = true
+        if (isReverse) {
+            //input from "to"(reverse)
+            setTokenBAmount(swapAmount)
+            setTokenAAmount(counterPairAmount)
+
+            if (counterPairAmount > myATokenBalance) {
+                isExceeded = true
+            }
+        } else {
+            //input from "from"(normal)
+            setTokenAAmount(swapAmount)
+            setTokenBAmount(counterPairAmount)
+
+            if (swapAmount > myATokenBalance) {
+                isExceeded = true
+            }
         }
 
-        this.setState({
-            [e.target.id]: e.target.value,
-            [counterPair]: Number(counterPairAmount).toFixed(2),
-            slippage: slippage,
-            isExceeded: isExceeded
-        });
+        setSlippage(slippage)
+        setIsExceeded(isExceeded)
 
         //helper 
         function getMyTokenBalanceNumber(denom, indexer) {
@@ -149,7 +135,7 @@ class Swap extends Component {
         }
     };
 
-    setPriceImpactRangeColor = (slippage) => {
+    function setPriceImpactRangeColor(slippage) {
         let color = "";
         if (slippage <= 1) {
             color = "rgb(39, 174, 96)";
@@ -163,19 +149,19 @@ class Swap extends Component {
         return { color: color };
     };
 
-    getTokenPrice = (a, b, reverse = false) => {
+    function getTokenPrice(a, b, reverse = false) {
         const price = b / a;
         if (price && price !== Infinity) {
             if (reverse) {
                 return (
                     <span>
-                        1 {this.state.tokenB.substr(1).toUpperCase()} = {parseFloat(price.toFixed(2))} {this.state.tokenA.substr(1).toUpperCase()}
+                        1 {tokenB.substr(1).toUpperCase()} = {parseFloat(price.toFixed(2))} {tokenA.substr(1).toUpperCase()}
                     </span>
                 );
             } else {
                 return (
                     <span>
-                        1 {this.state.tokenA.substr(1).toUpperCase()} = {parseFloat(price.toFixed(2))} {this.state.tokenB.substr(1).toUpperCase()}
+                        1 {tokenA.substr(1).toUpperCase()} = {parseFloat(price.toFixed(2))} {tokenB.substr(1).toUpperCase()}
                     </span>
                 );
             }
@@ -184,94 +170,80 @@ class Swap extends Component {
         }
     };
 
-    reset = () => {
-        this.setState({
-            tokenAAmount: "",
-            tokenBAmount: ""
-        });
+    function reset() {
+        setTokenAAmount("")
+        setTokenBAmount("")
+        setSlippage(0)
     };
 
-    selectPool = (item) => {
+    function selectPool(item) {
         console.log(item);
         if (item.liquidity_pool_metadata?.reserve_coins) {
-            this.setState({
-                isPoolSelected: !this.state.isPoolSelected,
-                poolId: item.liquidity_pool.pool_id,
-                poolTypeIndex: item.liquidity_pool.pool_type_index,
-                tokenA: item.liquidity_pool_metadata.reserve_coins[0].denom,
-                tokenB: item.liquidity_pool_metadata.reserve_coins[1].denom,
-                tokenAPoolAmount: item.liquidity_pool_metadata.reserve_coins[0].amount,
-                tokenBPoolAmount: item.liquidity_pool_metadata.reserve_coins[1].amount,
-                slippage: 0,
-                isExceeded: false
-            });
+            setIsPoolSelected(!isPoolSelected)
+            setPoolId(item.liquidity_pool.pool_id)
+            setPoolTypeIndex(item.liquidity_pool.pool_type_index)
+            setTokenA(item.liquidity_pool_metadata.reserve_coins[0].denom)
+            setTokenB(item.liquidity_pool_metadata.reserve_coins[1].denom)
+            setTokenAPoolAmount(item.liquidity_pool_metadata.reserve_coins[0].amount)
+            setTokenBPoolAmount(item.liquidity_pool_metadata.reserve_coins[1].amount)
+            setSlippage(0)
+            setIsExceeded(false)
         } else {
-            this.reset();
-            this.setState({
-                poolId: "",
-                poolTypeIndex: "",
-                tokenA: "",
-                tokenB: "",
-                tokenAAmount: "",
-                tokenBAmount: "",
-                tokenAPoolAmount: "",
-                tokenBPoolAmount: "",
-                slippage: 0,
-                isExceeded: false,
-                isPoolSelected: !this.state.isPoolSelected
-            });
+            reset();
+            setIsPoolSelected(!isPoolSelected)
+            setPoolId("")
+            setPoolTypeIndex("")
+            setTokenA("")
+            setTokenB("")
+            setTokenAPoolAmount("")
+            setTokenBPoolAmount("")
+            setIsExceeded(false)
         }
     };
 
-    tokenChange = () => {
-        let tokenA = this.state.tokenB;
-        let tokenB = this.state.tokenA;
-        let tokenAAmount = "";
-        let tokenBAmount = "";
-        let tokenAPoolAmount = this.state.tokenBPoolAmount;
-        let tokenBPoolAmount = this.state.tokenAPoolAmount;
-        this.setState({
-            slippage: 0,
-            tokenA: tokenA,
-            tokenB: tokenB,
-            tokenAAmount: tokenAAmount,
-            tokenBAmount: tokenBAmount,
-            tokenAPoolAmount: tokenAPoolAmount,
-            tokenBPoolAmount: tokenBPoolAmount
-        });
+    function tokenChange() {
+        const A = tokenB
+        const B = tokenA
+        const AP = tokenBPoolAmount
+        const BP = tokenAPoolAmount
+
+        setTokenA(A)
+        setTokenB(B)
+        setTokenAAmount("")
+        setTokenBAmount("")
+        setTokenAPoolAmount(AP)
+        setTokenBPoolAmount(BP)
+        setSlippage(0)
     };
 
-    render() {
-        const slippage = (this.state.slippage * 100).toFixed(2);
+    if (isPoolSelected) {
+        return (
+            <Wrapper>
+                <DepositCard>
+                    <GoBack onClick={selectPool}>
+                        <img src="/assets/arrow-left.svg" alt="left arrow" onClick={props.modalHandler} />
+                    </GoBack>
+                    <TokenSetter currencies={currencies} leftTitle="From" rightTitle={getMyTokenBalance(tokenA, tokenIndexer)} cssId="A" token={tokenA} tokenAmount={tokenAAmount} selectorHandler={tokenSelectorChangeHandler} amountHandler={amountChangeHandler} readOnly={true} />
+                    <ChangeButton func={tokenChange} />
+                    <TokenSetter currencies={currencies} leftTitle="To (estimated)" rightTitle={getMyTokenBalance(tokenB, tokenIndexer)} cssId="B" token={tokenB} tokenAmount={tokenBAmount} selectorHandler={tokenSelectorChangeHandler} amountHandler={amountChangeHandler} readOnly={true} />
 
-        if (this.state.isPoolSelected) {
-            return (
-                <Wrapper>
-                    <DepositCard>
-                        <GoBack onClick={this.selectPool}>
-                            <img src="/assets/arrow-left.svg" alt="left arrow" onClick={this.props.modalHandler} />
-                        </GoBack>
-                        <TokenSetter currencies={currencies} leftTitle="From" rightTitle={getMyTokenBalance(this.state.tokenA, this.state.tokenIndexer)} cssId="A" token={this.state.tokenA} tokenAmount={this.state.tokenAAmount} selectorHandler={this.tokenSelectorChangeHandler} amountHandler={this.amountChangeHandler} readOnly={true} />
-                        <ChangeButton func={this.tokenChange} />
-                        <TokenSetter currencies={currencies} leftTitle="To (estimated)" rightTitle={getMyTokenBalance(this.state.tokenB, this.state.tokenIndexer)} cssId="B" token={this.state.tokenB} tokenAmount={this.state.tokenBAmount} selectorHandler={this.tokenSelectorChangeHandler} amountHandler={this.amountChangeHandler} readOnly={true} />
-
-                        <BasicButtonCard function={this.swap} buttonName="SWAP" isLoading={this.state.isLoading} isDisabled={this.state.isExceeded}>
-                            <Detail>
-                                <div>Pool Price</div>
-                                <div>{this.getTokenPrice(this.state.tokenAPoolAmount, this.state.tokenBPoolAmount)}</div>
-                            </Detail>
-                            <Detail>
-                                <div>Price Impact</div>
-                                <div style={this.setPriceImpactRangeColor(slippage)}>{slippage}%</div>
-                            </Detail>
-                        </BasicButtonCard>
-                    </DepositCard>
-                </Wrapper>
-            );
-        } else {
-            return <PoolList poolsData={this.props.data.poolsData} selectPool={this.selectPool} actionType="Swap" />;
-        }
+                    <BasicButtonCard function={swap} buttonName="SWAP" isLoading={isLoading} isDisabled={isExceeded}>
+                        <Detail>
+                            <div>Pool Price</div>
+                            <div>{getTokenPrice(tokenAPoolAmount, tokenBPoolAmount)}</div>
+                        </Detail>
+                        <Detail>
+                            <div>Price Impact</div>
+                            <div style={setPriceImpactRangeColor((slippage * 100).toFixed(2))}>{(slippage * 100).toFixed(2)}%</div>
+                        </Detail>
+                    </BasicButtonCard>
+                </DepositCard>
+            </Wrapper>
+        );
+    } else {
+        return <PoolList poolsData={props.data.poolsData} selectPool={selectPool} actionType="Swap" />;
     }
+
 }
 
 export default Swap;
